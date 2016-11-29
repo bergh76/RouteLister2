101a524397afbf7884d3deac67e899218c20c9b2
Andreas <Andreas@DESKTOP-VR6U497>
2016-11-29 09:33:15 +0100

    Adding Roles control for login and right to user admin gui

--- RouteLister2/src/RouteLister2/Controllers/AdminController.cs
+++ RouteLister2/src/RouteLister2/Controllers/AdminController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RouteLister2.Models;
+using Microsoft.AspNetCore.Identity;
+using RouteLister2.Data;
+using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,8 +14,18 @@ namespace RouteLister2.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+        public AdminController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         // GET: /<controller>/
-        public IActionResult Index()
+        [Authorize(Roles ="Admin")]
+        public async Task<IActionResult> Index()
         {
             return View();
         }
