2a3d4a8f0d76b63a9951443ea2ae9be7cf6a5b0a
a b <anders9943@hotmail.com>
2016-11-29 11:36:12 +0100

    Added testdatasets, automapper. Cleaned up Unit test projects json and changed to Core 1.1 in main project json. Which all links have been removed from the web(try and find it i dare you)

---
+++ RouteLister2/src/RouteList2XUnitTests/ModelSchemaTests.cs
@@ -0,0 +1,199 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RouteLister2.Data;
+using RouteLister2.Models;
+using RouteLister2.Models.RouteListerViewModels;
+using System;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace RouteList2XUnitTests
+{
+    public class ModelSchemaTests
+    {
+        [Fact]
+        public void MustFail()
+        {
+            Assert.False(true);
+        }
+
+        [Fact]
+        //[InlineData("frk", " Kalle Anka\nParadisäppelvägen 111\n12345 Ankeborg")]
+        public void TestApplicationDBContextSchema()
+        {
+            //Arrange
+            var options = CreateNewContextOptions();
+            RouteList listOfDeliveries = FakeDataSets.DeliveryListFactory();
+
+
+
+            //Act
+            int result = 0;
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.RouteLists.Add(listOfDeliveries);
+                result = context.SaveChanges();
+
+            }
+
+            //Assert
+            Assert.Equal(13, result);
+        }
+        [Theory, MemberData(nameof(DeliveryLists), MemberType = typeof(ModelSchemaTests))]
+        public void DeliveryListViewModelShouldHaveCorrectData(RouteList deliveryList)
+        {
+            //Arrange
+            int Id = FakeDataSets.Id;
+            RouteListViewModel deliveryListViewModel = new RouteListViewModel()
+            {
+                Title = FakeDataSets.DeliveryListFactory().Title,
+                DeliveryListId = FakeDataSets.Id
+            };
+            using (var context = new ApplicationDbContext(CreateNewContextOptions()))
+            {
+                context.RouteLists.Add(deliveryList);
+                int changes = context.SaveChanges();
+                RouteListViewModel viewModelResult = null;
+                //Act
+                viewModelResult = context.RouteLists.Where(x => x.Id == Id).Select(
+                  x => new RouteListViewModel()
+                  {
+                      DeliveryListId = x.Id,
+                      Title = x.Title
+
+                  }).FirstOrDefault();
+                //Assert
+                Type t = viewModelResult.GetType();
+                var properties = t.GetProperties();
+
+                foreach (var item in properties)
+                {
+                    Assert.Equal(item.GetValue(viewModelResult), item.GetValue(deliveryListViewModel));
+                }
+            }
+        }
+        [Theory, MemberData(nameof(DeliveryLists), MemberType = typeof(ModelSchemaTests))]
+        public void OrderListViewModelShouldHaveCorrectData(RouteList deliveryList)
+        {
+
+            //Arrange
+            int Id = FakeDataSets.Id;
+            OrderDetailViewModel orderListViewModel = new OrderDetailViewModel()
+            {
+                FirstName = FakeDataSets.ContactFactory().FirstName,
+                LastName = FakeDataSets.ContactFactory().LastName,
+                Address = FakeDataSets.AddressFactory().Street,
+                PostNumber = FakeDataSets.AddressFactory().PostNumber,
+                City = FakeDataSets.AddressFactory().City,
+                PhoneNumbers = FakeDataSets.PhoneNumberFactory().Select(x => x.Number).ToList(),
+                OrderId = FakeDataSets.OrderFactory().Id,
+                DeliveryTypeName = FakeDataSets.OrderFactory().OrderType.Name
+            };
+            //TODO 
+            //Act
+            using (var context = new ApplicationDbContext(CreateNewContextOptions()))
+            {
+                context.RouteLists.Add(deliveryList);
+                context.SaveChanges();
+                OrderDetailViewModel resultingViewModel = null;
+                var result = from order in context.Orders.Where(x => x.Id == Id)
+                             from dest in context.Destinations.Where(x => x.Id == order.DestinationId)
+                             from cont in context.Contacts.Where(x => x.Id == dest.ContactId)
+                             from addr in context.Address.Where(x => x.Id == dest.AddressId)
+                             from orderType in context.OrderType.Where(x => x.Id == order.OrderTypeId)
+                             select new OrderDetailViewModel()
+                             {
+                                 FirstName = cont.FirstName,
+                                 LastName = cont.LastName,
+                                 Address = addr.Street,
+                                 City = addr.City,
+                                 PostNumber = addr.PostNumber,
+                                 PhoneNumbers = FakeDataSets.PhoneNumberFactory().Select(x => x.Number).ToList(),
+                                 OrderId = order.Id,
+                                 DeliveryTypeName = orderType.Name,
+                             };
+                resultingViewModel = result.FirstOrDefault();
+                //Assert
+                ReflectEvaluateObjectProperties(resultingViewModel, orderListViewModel);
+            }
+
+        }
+
+        private void ReflectEvaluateObjectProperties<T>(T t1, T t2)
+        {
+            Type t = t1.GetType();
+            var properties = t.GetProperties();
+
+
+            foreach (var item in properties)
+            {
+                Assert.Equal(item.GetValue(t1), item.GetValue(t2));
+            }
+        }
+
+        [Theory, MemberData(nameof(DeliveryLists), MemberType = typeof(ModelSchemaTests))]
+        public void OrderRowViewModelShouldHaveCorrectData(RouteList deliveryList)
+        {
+            //Arrange
+            OrderRowViewModel orderRowViewModel = new OrderRowViewModel()
+            {
+                OrderRowId = FakeDataSets.Id,
+                Count = FakeDataSets.OrderRowFactory().Count,
+                ParcelNumber = FakeDataSets.ParcelFactory().ParcelNumber,
+                ParcelName = FakeDataSets.ParcelFactory().Name,
+                //will probably want to add status like broken, so bool might be off the table
+                OrderRowStatus = FakeDataSets.OrderRowFactory().OrderRowStatus.Name == "Plockad"
+            };
+            //TODO
+            //Act
+            using (var context = new ApplicationDbContext(CreateNewContextOptions()))
+            {
+                context.RouteLists.Add(deliveryList);
+                context.SaveChanges();
+                OrderRowViewModel orderRowViewModelResult = null;
+                var result = from orderRow in context.OrderRows.Include(x => x.OrderRowStatus).Include(x => x.Parcel).Where(x => x.Id == FakeDataSets.OrderRowFactory().Id)
+                             select new OrderRowViewModel()
+                             {
+                                 Count = orderRow.Count,
+                                 OrderRowId = orderRow.Id,
+                                 OrderRowStatus = orderRow.OrderRowStatus.Name == "Plockad" ? true : false,
+                                 ParcelName = orderRow.Parcel.Name,
+                                 ParcelNumber = orderRow.Parcel.ParcelNumber
+                             };
+                orderRowViewModelResult = result.FirstOrDefault();
+
+                //Assert
+                ReflectEvaluateObjectProperties(orderRowViewModel, orderRowViewModelResult);
+
+            }
+        }
+        public static object[] DeliveryLists
+        {
+            get
+            {
+                return new[] { new object[] { FakeDataSets.DeliveryListFactory() } };
+            }
+        }
+
+        private static DbContextOptions<ApplicationDbContext> CreateNewContextOptions()
+        {
+            // Create a fresh service provider, and therefore a fresh 
+            // InMemory database instance.
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+
+
+            // Create a new options instance telling the context to use an
+            // InMemory database and the new service provider.
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            builder.UseInMemoryDatabase()
+                   .UseInternalServiceProvider(serviceProvider);
+
+
+            return builder.Options;
+        }
+    }
+}
---
+++ RouteLister2/src/RouteLister2/Data/FakeDataSets.cs
@@ -0,0 +1,145 @@
+﻿using RouteLister2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RouteLister2.Data
+{
+    public static class FakeDataSets
+    {
+        public static readonly int Id = 1;
+
+        public static RouteList DeliveryListFactory()
+        {
+            RouteList model = new RouteList();
+
+            model.Orders = new List<Order>() { OrderFactory() };
+            model.Title = "TestDeliveryList";
+            model.Vehicle = VehicleFactory();
+            model.VehicleId = model.Vehicle.Id;
+            model.Modified = DateTime.Now;
+            model.Created = DateTime.Parse("1999-01-01");
+            model.Assigned = DateTime.Now;
+            return model;
+        }
+
+        public static Vehicle VehicleFactory()
+        {
+            Vehicle model = new Vehicle()
+            {
+                Id = Id,
+                RegistrationNumber = "xxx111"
+            };
+            return model;
+        }
+
+        public static Order OrderFactory()
+        {
+            Order model = new Order();
+            model.Id = Id;
+            model.Destination = DestinationFactory();
+            model.OrderRows = new List<OrderRow>();
+            model.OrderRows.Add(OrderRowFactory());
+            model.OrderStatus = OrderStatusFactory();
+            model.OrderStatusId = model.OrderStatus.Id;
+            model.OrderType = OrderTypeFactory();
+            model.OrderTypeId = model.OrderType.Id;
+            return model;
+
+        }
+
+        public static OrderType OrderTypeFactory()
+        {
+            OrderType model = new OrderType();
+            model.Id = Id;
+            model.Description = "TestOrderType";
+            model.Name = "Curbside";
+            return model;
+        }
+
+        public static OrderStatus OrderStatusFactory()
+        {
+            OrderStatus model = new OrderStatus();
+            model.Id = Id;
+            model.Name = "Leverad";
+            model.Description = "Ordern är levererad";
+            model.Priority = 1;
+            return model;
+        }
+
+        public static Destination DestinationFactory()
+        {
+            Destination model = new Destination();
+            model.Id = Id;
+            model.Address = AddressFactory();
+            model.AddressId = model.Address.Id;
+            model.Contact = ContactFactory();
+            model.ContactId = model.Contact.Id;
+
+            return model;
+        }
+
+        public static Contact ContactFactory()
+        {
+            Contact model = new Contact();
+            model.Id = Id;
+            model.FirstName = "Klas";
+            model.LastName = "Knutsson";
+            model.PhoneNumbers = PhoneNumberFactory();
+            return model;
+
+        }
+        public static List<PhoneNumber> PhoneNumberFactory()
+        {
+            return new List<PhoneNumber>()
+            {
+                    new PhoneNumber() {  Id = Id,Number = "0499 - 12 34 56" },
+                    new PhoneNumber() {Id=Id+1,Number = "0703 - 22 33 44" }
+            };
+        }
+        public static Address AddressFactory()
+        {
+
+            Address model = new Address();
+            model.Id = Id;
+            model.City = "Nyköping";
+            model.County = "Stockholms Län";
+
+            model.PostNumber = "392 44";
+            model.Street = "Björkkvistsgatan 3";
+
+            return model;
+
+        }
+
+        public static OrderRow OrderRowFactory()
+        {
+            OrderRow model = new OrderRow();
+            model.Id = Id;
+            model.OrderRowStatus = OrderRowStatusFactory();
+            model.OrderRowStatusId = model.OrderRowStatus.Id;
+            model.Parcel = ParcelFactory();
+            model.ParcelId = model.Parcel.Id;
+            model.Count = 1;
+            return model;
+        }
+
+        public static Parcel ParcelFactory()
+        {
+            Parcel model = new Parcel();
+            model.Id = Id;
+            model.Name = "Cylinda Diskmaskin";
+            model.ParcelNumber = "078282988529SE";
+            return model;
+        }
+
+        public static OrderRowStatus OrderRowStatusFactory()
+        {
+            OrderRowStatus model = new OrderRowStatus();
+            model.Id = Id;
+            model.Name = "Plockad";
+            return model;
+        }
+    }
+}
