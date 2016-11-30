92d3d186ac818775ab6be86fbae9c2afd2fcafd6
Andreas <Andreas@DESKTOP-OIF9A29>
2016-11-30 22:05:42 +0100

    Testing

--- RouteLister2/src/RouteLister2/Services/SignalR/PostRepository.cs
+++ RouteLister2/src/RouteLister2/Services/SignalR/PostRepository.cs
@@ -1,4 +1,5 @@
 ﻿using RouteLister2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,11 +9,12 @@ namespace RouteLister2.SignalR
     {
         private List<Post> _posts = new List<Post>()
         {
-            new Post(1, "Obi-Wan Kenobi","These are not the droids you're looking for","10:22:12"),
-            new Post(2, "Darth Vader","I find your lack of faith disturbing","11:31:56")
+            //new Post(1, "Obi-Wan Kenobi","These are not the droids you're looking for","10:22:12"),
+            //new Post(2, "Darth Vader","I find your lack of faith disturbing","11:31:56")
         };
         public void AddPost(Post post)
         {
+            post.Time = DateTime.Now.ToString("yyyy-mm-dd HH:mm:ss");
             _posts.Add(post);
         }
 
--- RouteLister2/src/RouteLister2/wwwroot/js/site.js
+++ RouteLister2/src/RouteLister2/wwwroot/js/site.js
@@ -1,125 +1,140 @@
 ﻿// Write your Javascript code.
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =
+        h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
+}
 /* Formatting function for row details - modify as you need */
-//function format(d) {
-//    // `d` is the original data object for the row
-//    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
-//       '<tr>' +
-//            '<td>Datum:</td>1' +
-//            '<td><input class="form-control" type="text" id="date" value="' + d.Datum + '"  /></td>' +
-//       '</tr>' +
-//       '<tr>' +
-//            '<td>Antal:</td>' +
-//            '<td><input class="form-control" type="text" id="amount" value="' + d.Antal + '"  /></td>' +
-//       '</tr>' +
-//       '<tr>' +
-//             '<td>Namn:</td>' +
-//            '<td><input class="form-control" type="text" id="name" value="' + d.Namn + '"  /></td>' +
-//       '</tr>' +
-//       '<tr>' +
-//            '<td>Adress:</td>' +
-//            '<td><input class="form-control" type="text" id="adress" value="' + d.Adress + '"  /></td>' +
-//       '</tr>' +
-//       '<tr>' +
-//            '<td>Stad:</td>' +
-//            '<td><input class="form-control" type="text" id="sity" value="' + d.Stad + '"  /></td>' +
-//       '</tr>' +
-//       '<tr>' +
-//            '<td>Postnr:</td>' +
-//            '<td><input class="form-control" type="text" id="areacode" value="' + d.Postnr + '"  /></td>' +
-//       '</tr>' +
-//       '<tr>' +
-//            '<td>Leverantor:</td>' +
-//            '<td><input class="form-control" type="text" id="supplyer" value="' + d.Leverantor + '"  /></td>' +
-//       '</tr>' +
-//       '<tr>' +
-//            '<td>KollieId:</td>' +
-//            '<td><input class="form-control" type="text" id="collieid" value="' + d.KollieId + '"  /></td>' +
-//       '</tr>' +
-//       '<tr>' +
-//            '<td>Artikel:</td>' +
-//            '<td><input class="form-control" type="text" id="article" value="' + d.Artikel + '"  /></td>' +
-//       '</tr>' +
-//       '<tr>' +
-//            '<td>Tilldelad:</td>' +
-//            '<td><input class="form-control" type="text" id="assigned" value="' + d.Tilldelad + '"  /></td>' +
-//       '</tr>' +
-//       '<tr>' +
-//            '<td>Spara:</td><td><button class="btn btn-xs btn-success fa fa-save pull-right"></button></td>' +
-//       '</tr>' +
-//       '</table>';
-//}
-//// < datatables VEHICLE 1 API start
-//$(document).ready(function () {
-//    //$('#carOne').DataTable({
-//    var table = $('#carOne').DataTable({
-//        "ajax": "../testdata/testdatatables.json",
-//        "columns": [
-//            {
-//                "className": 'details-control',
-//                "orderable": false,
-//                "data": null,
-//                "defaultContent": ''
-//            },
-//            { "data": "Datum" },
-//            { "data": "Antal" },
-//            { "data": "Namn" },
-//            { "data": "Adress" },
-//            { "data": "Stad" },
-//            { "data": "Postnr" },
-//            { "data": "Leverantor" },
-//            { "data": "KollieId" },
-//            { "data": "Artikel" },
-//            { "data": "Tilldelad" },
-//            {
-//                "className": 'details-control',
-//                "orderable": false,
-//                "data": null,
-//                "defaultContent": ''
-//            }
-//        ],
-//        dom: 'B<"clear"><lf<t>ip>',
-//        buttons: [{ extend: 'copy', text: 'Kopiera', },
-//            {
-//                extend: 'excel',
-//                text: 'Excel',
-//                exportOptions: { modifier: { page: 'current' } },
-//            },
-//            {
-//                //extend: 'pdf',
-//                extend: 'pdfHtml5',
-//                text: 'Pdf',
-//                orientation: 'landscape',
-//                pageSize: 'LEGAL'
-//            },
-//        ],
-//        "language": {
-//            "lengthMenu": "Visa _MENU_ rader per sida",
-//            "zeroRecords": "Din sökning gav inget - försök med ett annat sökord",
-//            "info": "_PAGE_ av _PAGES_",
-//            "infoEmpty": "Datatabellen är tom",
-//            "infoFiltered": "(filtrerdad på _MAX_ antal poster)"
-//        },
-//        "processing": true,
-//        "order": [[1, "asc"], [6, 'asc']]
-//    })
-//    console.log(table);
-//    $('#carOne tbody').on('click', 'td.details-control', function () {
-//        var tr = $(this).closest('tr');
-//        var row = table.row(tr);
+function format(d) {
+    // `d` is the original data object for the row
+    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
+       '<tr>' +
+            '<td>Datum:</td>1' +
+            '<td><input class="form-control" type="text" id="date" value="' + d.Datum + '"  /></td>' +
+       '</tr>' +
+       '<tr>' +
+            '<td>Antal:</td>' +
+            '<td><input class="form-control" type="text" id="amount" value="' + d.Antal + '"  /></td>' +
+       '</tr>' +
+       '<tr>' +
+             '<td>Namn:</td>' +
+            '<td><input class="form-control" type="text" id="name" value="' + d.Namn + '"  /></td>' +
+       '</tr>' +
+       '<tr>' +
+            '<td>Adress:</td>' +
+            '<td><input class="form-control" type="text" id="adress" value="' + d.Adress + '"  /></td>' +
+       '</tr>' +
+       '<tr>' +
+            '<td>Stad:</td>' +
+            '<td><input class="form-control" type="text" id="sity" value="' + d.Stad + '"  /></td>' +
+       '</tr>' +
+       '<tr>' +
+            '<td>Postnr:</td>' +
+            '<td><input class="form-control" type="text" id="areacode" value="' + d.Postnr + '"  /></td>' +
+       '</tr>' +
+       '<tr>' +
+            '<td>Leverantor:</td>' +
+            '<td><input class="form-control" type="text" id="supplyer" value="' + d.Leverantor + '"  /></td>' +
+       '</tr>' +
+       '<tr>' +
+            '<td>KollieId:</td>' +
+            '<td><input class="form-control" type="text" id="collieid" value="' + d.KollieId + '"  /></td>' +
+       '</tr>' +
+       '<tr>' +
+            '<td>Artikel:</td>' +
+            '<td><input class="form-control" type="text" id="article" value="' + d.Artikel + '"  /></td>' +
+       '</tr>' +
+       '<tr>' +
+            '<td>Tilldelad:</td>' +
+            '<td><input class="form-control" type="text" id="assigned" value="' + d.Tilldelad + '"  /></td>' +
+       '</tr>' +
+       '<tr>' +
+            '<td>Spara:</td><td><button class="btn btn-xs btn-success fa fa-save pull-right"></button></td>' +
+       '</tr>' +
+       '</table>';
+}
+// < datatables VEHICLE 1 API start
+$(document).ready(function () {
+    //$('#carOne').DataTable({
+    var table = $('#carOne').DataTable({
+        "ajax": "../testdata/testdatatables.json",
+        "columns": [
+            {
+                "className": 'details-control',
+                "orderable": false,
+                "data": null,
+                "defaultContent": ''
+            },
+            { "data": "Datum" },
+            { "data": "Antal" },
+            { "data": "Namn" },
+            { "data": "Adress" },
+            { "data": "Stad" },
+            { "data": "Postnr" },
+            { "data": "Leverantor" },
+            { "data": "KollieId" },
+            { "data": "Artikel" },
+            { "data": "Tilldelad" },
+            {
+                "className": 'details-control',
+                "orderable": false,
+                "data": null,
+                "defaultContent": ''
+            }
+        ],
+        dom: 'B<"clear"><lf<t>ip>',
+        buttons: [{ extend: 'copy', text: 'Kopiera', },
+            {
+                extend: 'excel',
+                text: 'Excel',
+                exportOptions: { modifier: { page: 'current' } },
+            },
+            {
+                //extend: 'pdf',
+                extend: 'pdfHtml5',
+                text: 'Pdf',
+                orientation: 'landscape',
+                pageSize: 'LEGAL'
+            },
+        ],
+        "language": {
+            "lengthMenu": "Visa _MENU_ rader per sida",
+            "zeroRecords": "Din sökning gav inget - försök med ett annat sökord",
+            "info": "_PAGE_ av _PAGES_",
+            "infoEmpty": "Datatabellen är tom",
+            "infoFiltered": "(filtrerdad på _MAX_ antal poster)"
+        },
+        "processing": true,
+        "order": [[1, "asc"], [6, 'asc']]
+    })
+    console.log(table);
+    $('#carOne tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = table.row(tr);
 
-//        if (row.child.isShown()) {
-//            // This row is already open - close it
-//            row.child.hide();
-//            tr.removeClass('shown');
-//        }
-//        else {
-//            // Open this row
-//            row.child(format(row.data())).show();
-//            tr.addClass('shown');
-//        }
-//    });
-//});
-//// datatables API end
+        if (row.child.isShown()) {
+            // This row is already open - close it
+            row.child.hide();
+            tr.removeClass('shown');
+        }
+        else {
+            // Open this row
+            row.child(format(row.data())).show();
+            tr.addClass('shown');
+        }
+    });
+});
+// datatables API end
 
 //// < datatables VEHICLE 2 API start
 //$(document).ready(function () {
