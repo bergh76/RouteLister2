ed886bd153ac03cdbe94fbf52b89e2b80829a060
Andreas <Andreas@DESKTOP-OIF9A29>
2016-12-01 17:19:44 +0100

    Added ViewComponent for listing Users in Admin/RegisterUser/Users

---
+++ RouteLister2/src/RouteLister2/Models/UsersInSystem.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RouteLister2.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RouteLister2.Models
+{
+    public class UsersInSystem
+    {
+        //public string Role { get; set;}
+        //public ApplicationUser User { get; set; }
+        private static ApplicationDbContext _context;
+        public IEnumerable<ApplicationUser> _users { get; set; }
+        public UsersInSystem(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ApplicationUser>> GetAllUsers()
+        {
+            return _context.Users;
+        }
+    }
+}
