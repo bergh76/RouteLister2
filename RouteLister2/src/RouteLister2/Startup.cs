01565d54e1f05bf52965a56b58ab41acbc8b2d49
Andreas <Andreas@DESKTOP-OIF9A29>
2016-12-20 08:35:54 +0100

    somethings wrong with routing

--- RouteLister2/src/RouteLister2/Startup.cs
+++ RouteLister2/src/RouteLister2/Startup.cs
@@ -43,7 +43,7 @@ namespace RouteLister2
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddTransient<SeedDefaultData>();
+            //services.AddTransient<SeedDefaultData>();
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
@@ -87,11 +87,11 @@ namespace RouteLister2
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, SeedDefaultData seedUser)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) //, SeedDefaultData seedUser)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
-            seedUser.SeedAdminUser();
+            //seedUser.SeedAdminUser();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
